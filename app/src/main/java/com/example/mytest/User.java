be993e0601cd97b38733c0b0c73bfd21e768798c
chaehwanli <chaehwan.li@gmail.com>
2023-04-17 22:22:21 +0900

    https://github.com/chaehwanli/mockito-testcode/issues/3

---
+++ app/src/main/java/com/example/mytest/User.java
@@ -0,0 +1,39 @@
+package com.example.mytest;
+
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof User)) {
+            return false;
+        }
+        User user = (User) obj;
+        return firstName.equals(user.firstName) && lastName.equals(user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        return result;
+    }
+}
